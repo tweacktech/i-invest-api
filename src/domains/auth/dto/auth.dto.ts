c196b630eb7f8dab84703ed8fcc45a7b848ec29e
daridaniel <dari.daniel@moneta.ng>
2026-06-06 16:21:13 +0100

    updated rule

--- src/domains/auth/dto/auth.dto.ts
+++ src/domains/auth/dto/auth.dto.ts
@@ -2,11 +2,12 @@ import { IsOptional, IsString, Matches, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
-  @Matches(/^\+234\d{10}$/, { message: 'Phone must be in +234XXXXXXXXXX format' })
+  @Matches(/^(\+234|0)\d{10}$/, {
+    message: 'Phone must be in 080XXXXXXXX, 090XXXXXXXX or +234XXXXXXXXXX format'})
   phoneNumber!: string;
 
   @IsString()
-  @MinLength(8)
+  @MinLength(6)
   @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/, {
     message: 'Password must contain uppercase, number and special character',
   })
